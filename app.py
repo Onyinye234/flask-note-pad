5e147878ee26dfbad28e41177458f530ecb20726
Onyinyechi <ayituonyinyechi@gmail.com>
2025-07-01 18:35:30 -0700

    create

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+db = SQLAlchemy(app)
+
+
+class Notes(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable = False)
+    content = db.Column(db.String(5000), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return f"Note {self.id}"
+
+@app.route('/')
+
+
+def index():
+    db.create_all()
+    
+    if request.method == 'POST':
+        note_title = request.form['title']
+        note_content = request.form['content']
+
+        new_note = Notes(title = note_title, content = note_content)
+
+        try:
+            db.session.add(new_note)
+            db.session.commit
+            return redirect('/')
+
+        except:
+            return "Error occurred while creating note"
+    else:
+        all_notes = Notes.query.order_by(Notes.date_created).all()
+        return render_template('index.html', notes = all_notes)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
